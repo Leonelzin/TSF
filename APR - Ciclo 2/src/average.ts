c4537a4e3c5b60527e52a590183f6d1e33d5aea1
Leonel <DouglasLuis2017@outlook.com.br>
2024-10-30 13:43:04 -0300

    Fase Red: Testes falhando

--- APR - Ciclo 2/src/average.ts
+++ APR - Ciclo 2/src/average.ts
@@ -1,3 +1,6 @@
-//Aluno: Douglas Leonel de Almeida - 2110213
+// Aluno: Douglas Leonel de Almeida - 2110213
 
-//empty
+export function calculateAverage(n1: number, n2: number, n3: number): number {
+    // Alterando a lógica da média para garantir que os testes falhem
+    return (n1 + n2 + n3) / 2; // Dividir por 2 ao invés de 3
+}
